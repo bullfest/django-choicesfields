3d8daf6c784ea3860c901c89d6723f8ec1d7e514
Alexander Viklund <bullfest@sthlm.dev>
2020-08-29 16:15:15 +0200

    Add sqlite-database to settings and migrate

--- tests/test_app/settings.py
+++ tests/test_app/settings.py
@@ -5,3 +5,14 @@ Dummy settings for test app
 SECRET_KEY = "supersecret"
 
 INSTALLED_APPS = ["tests.test_app"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "CONN_MAX_AGE": 0,
+        "NAME": ":memory:",
+        "OPTIONS": {
+            "timeout": 20,
+        },
+    }
+}
